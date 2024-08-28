38bb068dc6ee32039fa29660d94528e2e8c61ddb
MShayanShakeel <mshayanshakeel786@gmail.com>
2024-08-29 02:13:37 +0500

    updatte code

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -249,7 +249,7 @@ const Menu = () => {
                   height={50}
                   src="https://marketplace.canva.com/EAFuJ5pCLLM/1/0/1600w/canva-black-and-gold-simple-business-man-linkedin-profile-picture-BM_NPo97JwE.jpg"
                   alt="Profile-picture"
-                  style={{ marginRight: "5px", marginLeft: "-2rem" }}
+                  style={{ marginLeft: "-9rem" }}
                 />
                 {userID?.email}
                 <MdMoreHoriz
